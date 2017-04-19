29e0e5de01388d8ad8bf98030304b23b40f1f14e
Michal Switalik <michal.switalik@gmail.com>
2017-04-19 19:47:26 +0200

    Add project files.

---
+++ AppLibrary/Translator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace AppLibrary
+{
+    public class Translator
+    {
+        public List<Sound> TranslateToMorse(string v)
+        {
+            return new List<Sound>()
+            {
+                Sound.Dot, Sound.Dot, Sound.Dot, Sound.PauseCharacter,
+                Sound.Dash,Sound.Dash,Sound.Dash,Sound.PauseCharacter,
+                Sound.Dot, Sound.Dot, Sound.Dot, Sound.PauseCharacter,
+            };
+        }
+    }
+}
