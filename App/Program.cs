7d2c41328c11d17a2807905c2fd621dd1d959f37
Michał Świtalik <michal.switalik@gmail.com>
2017-04-23 08:33:51 +0200

    Finished program

--- App/Program.cs
+++ App/Program.cs
@@ -7,9 +7,15 @@ namespace App
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Write your message and confirm with enter: ");
+            var message = Console.ReadLine();
+
             Translator translator = new Translator();
             Beeper beeper = new Beeper();
-            beeper.TransmitSounds(translator.TranslateToMorse("S O S"));
+
+            Console.WriteLine("Starting to transmit message...");
+            beeper.TransmitSounds(translator.TranslateToMorse(message));
+            Console.WriteLine("Finished. Press any key to exit");
             Console.ReadKey();
         }
     }
