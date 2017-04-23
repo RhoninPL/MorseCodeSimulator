f90f27eba06a4628024b5dfa261a5a886a746d5e
Michał Świtalik <michal.switalik@gmail.com>
2017-04-23 08:33:51 +0200

    Finished program

--- AppLibrary/Translator.cs
+++ AppLibrary/Translator.cs
@@ -1,17 +1,28 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace AppLibrary
 {
     public class Translator
     {
-        public List<Sound> TranslateToMorse(string v)
+        public List<Sound> TranslateToMorse(string message)
         {
-            return new List<Sound>()
+            var morseCode = new List<Sound>();
+
+            foreach (var characeter in message.ToUpper())
             {
-                Sound.Dot, Sound.Dot, Sound.Dot, Sound.PauseCharacter,
-                Sound.Dash,Sound.Dash,Sound.Dash,Sound.PauseCharacter,
-                Sound.Dot, Sound.Dot, Sound.Dot, Sound.PauseCharacter,
-            };
+                var sound = new List<Sound>();
+                if (MorseCode.morseCode.TryGetValue(characeter, out sound))
+                {
+                    morseCode.AddRange(sound);
+                    
+                    morseCode.Add(characeter == ' ' ? Sound.PauseWord : Sound.PauseElement);
+                }
+                else
+                    Console.WriteLine($"Cannot transmit: {characeter}");
+            }
+
+            return morseCode;
         }
     }
 }
